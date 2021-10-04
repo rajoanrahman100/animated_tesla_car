dcd314628d21dcfe12b80bd7574da9bb72e0d383
rajoanrahman100 <rajoan@rokkhi.com>
2021-10-04 15:23:59 +0600

    commit from master

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   Widget build(BuildContext context) {
 
-    //Change from dev_rejoaan
+
 
     return AnimatedBuilder(
         animation: Listenable.merge([homeController, _batteryAnimationController]),
